37e316a12c76e9f62be67af4e4d7a8609ad004d2
Shane Creighton-Young <srcreigh@uwaterloo.ca>
2015-04-14 21:55:32 -0700

    Update example program

--- test/src/main/java/com/example/Main.java
+++ test/src/main/java/com/example/Main.java
@@ -1,35 +1,36 @@
 package com.example;
 
-import com.qklabs.Actor;
-import com.qklabs.ActorRef;
-import com.qklabs.ActorSystem;
+import com.qklabs.actor.Actor;
+import com.qklabs.actor.ActorRef;
+import com.qklabs.actor.ActorSystem;
 
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ActorSystem system = new ActorSystem();
-        ActorRef actor1 = system.createActor(TestActor.class);
-        ActorRef actor2 = system.createActor(TestActor.class);
+        ActorRef actor1 = system.getOrCreateActor(TestActor.class, "/actor1");
+        ActorRef actor2 = system.getOrCreateActor(TestActor.class, "/actor2");
 
         for (int i = 0; i < 20; i++) {
             if (i % 2 == 0) {
-                System.out.println("sent message to actor1");
                 actor1.tell("Hello! " + i);
             } else {
-                System.out.println("sent message to actor2");
                 actor2.tell("Hey there! " + i);
             }
         }
 
-        while (true);
+        Thread.sleep(5000);
+
+        System.out.println("Too late!");
+        System.exit(0);
     }
 
     public static class TestActor extends Actor {
         Random random = new Random();
         public void onReceive(Object message, ActorRef sender) {
             try {
-                Thread.sleep(10 * random.nextInt(10));
+                Thread.sleep(10 * random.nextInt(100));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
